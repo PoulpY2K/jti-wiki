f385d89a1d8f8e3840a51b82a0bb4da4015544f8
Jérémy Laurent <poulpy2k@air-de-jeremy.home>
2023-09-11 19:36:46 +0200

    feat(pwa): add pwa support

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { pwaInfo } from 'virtual:pwa-info';
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+</script>
+
+<svelte:head>
+	{@html webManifestLink}
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+{#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt }}
+	<ReloadPrompt />
+{/await}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,45 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.svg', 'robots.txt', 'sveltekit-pwa.js', 'apple-touch-icon.png'],
+			devOptions: {
+				enabled: true
+			},
+			manifest: {
+				name: 'Jujutsu Tech International Wiki',
+				short_name: 'JTI Wiki',
+				theme_color: '#8b0000',
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any'
+					},
+					{
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					}
+				]
+			}
+		})
+	]
 });
